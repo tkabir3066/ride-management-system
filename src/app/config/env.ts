d970572310e18d2b8765de1897ae54dcecdb35e2
Tutul Kabir <tutulkabir@gmail.com>
2025-08-29 14:47:01 +0530

    configured .env file and handled Unhandled rejection erro, Uncaught exception error etc

---
+++ src/app/config/env.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EnvConfig {
+  PORT: string;
+  MONGO_URI: string;
+  NODE_ENV: "development" | "production";
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables: string[] = ["PORT", "MONGO_URI", "NODE_ENV"];
+
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing required environment variable ${key}`);
+    }
+  });
+
+  return {
+    PORT: process.env.PORT as string,
+    MONGO_URI: process.env.MONGO_URI as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+  };
+};
+
+export const envVars = loadEnvVariables();
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { Server } from "http";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 dotenv.config();
 
@@ -9,11 +10,11 @@ let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(envVars.MONGO_URI);
     console.log("MongoDB connected successfully");
 
-    server = app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port: ${process.env.PORT}`);
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is running on port: ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
@@ -21,3 +22,46 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM Signal received...server shutting down..");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+process.on("SIGINT", () => {
+  console.log("SIGINT Signal received...server shutting down..");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled rejection detected...server shutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught exception detected...server shutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
